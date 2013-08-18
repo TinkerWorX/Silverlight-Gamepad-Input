2e27b9aa12a2a495eaac3a5b01566b9d05ad82f9
TinkerWorX <nikolaj.mariager@gmail.com>
2013-08-18 22:56:35 +0200

    Major refactor. Turns out the initial code didn't work at all. This should now work with Firefox Nightly and Chrome Canary. They have both been tested on Windows 7 using a USB XBox 360 controller. Also added some more things to the sample and disabled out-of-browser as you can't bridge JavaScript in those cases.

--- TinkerWorX.Silverlight.Input/GamePad.cs
+++ TinkerWorX.Silverlight.Input/GamePad.cs
@@ -22,176 +22,312 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 using System;
+using System.Collections.Generic;
 using System.Windows.Browser;
 
 namespace TinkerWorX.Silverlight.Input
 {
     public static class GamePad
     {
-        private static readonly Boolean IsWindows = HtmlPage.BrowserInformation.UserAgent.Contains("Windows NT");
-        private static readonly Boolean IsMacintosh = HtmlPage.BrowserInformation.UserAgent.Contains("Macintosh");
+        private static GamePadOperatingSystem OperatingSystem { get; set; }
 
-        private static readonly Boolean IsChrome = HtmlPage.BrowserInformation.UserAgent.Contains("Chrome/");
-        private static readonly Boolean IsFirefox = HtmlPage.BrowserInformation.UserAgent.Contains("Firefox/");
+        private static GamePadBrowser Browser { get; set; }
 
-        public static GamePadState GetState(Int32 index, HtmlWindow window)
+        public static GamePadSupport Support { get; private set; }
+
+        static GamePad()
         {
-            var gamepads = (HtmlPage.Window.Eval("navigator.webkitGamepads || navigator.mozGamepads || navigator.gamepads") as ScriptObject);
-            if (gamepads == null)
-                throw new NotSupportedException("This browser does not appear to support gamepads. Only the latest dev version of Firefox or Chrome supports gamepads so far.");
+            // Detect operating system
+            if (HtmlPage.BrowserInformation.UserAgent.Contains("Windows NT"))
+                OperatingSystem = GamePadOperatingSystem.Windows;
+            else if (HtmlPage.BrowserInformation.UserAgent.Contains("Macintosh"))
+                OperatingSystem = GamePadOperatingSystem.Macintosh;
+
+            if (OperatingSystem == GamePadOperatingSystem.Unknown)
+            {
+                Support = GamePadSupport.Unsupported;
+                return;
+            }
+
+            // Detect browser
+            if (HtmlPage.BrowserInformation.UserAgent.Contains("Chrome/"))
+                Browser = GamePadBrowser.Chrome;
+            else if (HtmlPage.BrowserInformation.UserAgent.Contains("Firefox/"))
+                Browser = GamePadBrowser.Firefox;
+
+            if (Browser == GamePadBrowser.Unknown)
+            {
+                Support = GamePadSupport.Unsupported;
+                return;
+            }
+
+            // Call the correct initializer
+            switch (Browser)
+            {
+                case GamePadBrowser.Chrome:
+                    Chrome_Initialize();
+                    break;
+
+                case GamePadBrowser.Firefox:
+                    Firefox_Initialize();
+                    break;
+            }
+
+            // Call the common initializer
+            Common_Initialize();
+        }
+
+        #region Chrome
+
+        private static ScriptObject Chrome_GetGamePads()
+        {
+            if ((HtmlPage.Window.Eval("navigator.webkitGetGamepads()") as ScriptObject) != null) // Chrome 22 and newer
+                return (HtmlPage.Window.Eval("navigator.webkitGetGamepads()") as ScriptObject);
+            if ((HtmlPage.Window.Eval("navigator.webkitGamepads") as ScriptObject) != null) // Chrome 21
+                return (HtmlPage.Window.Eval("navigator.webkitGamepads") as ScriptObject);
+            return null;
+        }
+
+        private static ScriptObject Chrome_GetGamePad(Int32 index)
+        {
+            return (Chrome_GetGamePads().GetProperty(index) as ScriptObject);
+        }
+
+        private static void Chrome_Initialize()
+        {
+            if (Chrome_GetGamePads() == null)
+            {
+                Support = GamePadSupport.Unsupported;
+                return;
+            }
+
+            // Do we need to initialize more?
+        }
+
+        #endregion Chrome
+
+        #region Firefox
+
+        private static Dictionary<Int32, ScriptObject> Firefox_GamePads = new Dictionary<Int32, ScriptObject>();
+
+        private static ScriptObject Firefox_GetGamePad(Int32 index)
+        {
+            var gamepad = (ScriptObject)null;
+            if (Firefox_GamePads.TryGetValue(index, out gamepad))
+                return gamepad;
+            return null;
+        }
+
+        private static void Firefox_Initialize()
+        {
+            // Currently there is no way to detect gamepad support in Firefox.
+            Support = GamePadSupport.Unknown;
+
+            HtmlPage.Window.AttachEvent("MozGamepadConnected", new EventHandler<HtmlEventArgs>(delegate(Object s, HtmlEventArgs e)
+            {
+                var gamepad = (e.EventObject.GetProperty("gamepad") as ScriptObject);
+                var index = (Int32)(Double)gamepad.GetProperty("index");
+                Firefox_GamePads.Add(index, gamepad);
+            }));
+            HtmlPage.Window.AttachEvent("MozGamepadDisconnected", new EventHandler<HtmlEventArgs>(delegate(Object s, HtmlEventArgs e)
+            {
+                var gamepad = (e.EventObject.GetProperty("gamepad") as ScriptObject);
+                var index = (Int32)(Double)gamepad.GetProperty("index");
+                Firefox_GamePads.Remove(index);
+            }));
+        }
+
+        #endregion Firefox
+
+        #region Common
+
+        private static ScriptObject Common_GetGamePad(Int32 index)
+        {
+            switch (Browser)
+            {
+                case GamePadBrowser.Chrome: return Chrome_GetGamePad(index);
+                case GamePadBrowser.Firefox: return Firefox_GetGamePad(index);
+                default: return null;
+            }
+        }
+
+        private static void Common_Initialize()
+        {
+
+        }
+
+        #endregion Common
+
+        public static GamePadState GetState(Int32 index)
+        {
+            if (Support == GamePadSupport.Unsupported)
+                throw new NotSupportedException("This browser does not appear to support gamepads.");
+
 
             var gamepadState = new GamePadState(index);
 
-            var gamepad = (gamepads.GetProperty(index) as ScriptObject);
+            var gamepad = Common_GetGamePad(index);
             if (gamepad == null)
                 return gamepadState;
             var identifier = (gamepad.GetProperty("id") as String);
 
-            if (IsWindows)
+            switch (OperatingSystem)
             {
-                if (IsChrome)
-                {
-                    if (identifier.Contains("XInput")) // XInput
+                case GamePadOperatingSystem.Windows:
+                    switch (Browser)
                     {
-                        if (identifier.Contains("GAMEPAD")) // GAMEPAD
-                        {
-                            gamepadState.ReadAsWindowsChromeXInput(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
+                        case GamePadBrowser.Chrome:
+                            if (identifier.Contains("XInput")) // XInput
+                            {
+                                if (identifier.Contains("GAMEPAD")) // GAMEPAD
+                                {
+                                    gamepadState.ReadAsWindowsChromeXInput(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else
+                            {
+                                throw new NotImplementedException(identifier);
+                            }
+                            break;
+
+                        case GamePadBrowser.Firefox: 
+                            if (identifier.Contains("45e-")) // Microsoft
+                            {
+                                if (identifier.Contains("28e-") || identifier.Contains("2a1-")) // XBox 360 controller
+                                {
+                                    gamepadState.ReadAsWindowsFirefoxXBox360(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else if (identifier.Contains("46d-")) // Logitech
+                            {
+                                if (identifier.Contains("c21d-")) // F310 controller
+                                {
+                                    gamepadState.ReadAsWindowsFirefoxF310(gamepad);
+                                }
+                                else if (identifier.Contains("c21e-")) // F510 controller
+                                {
+                                    gamepadState.ReadAsWindowsFirefoxF510(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else
+                            {
+                                throw new NotImplementedException(identifier);
+                            }
+                            break;
+
+                        case GamePadBrowser.Unknown:
+                            throw new NotSupportedException("Unsupported browser.");
                     }
-                    else
+                    break;
+
+                case GamePadOperatingSystem.Macintosh:
+                    switch (Browser)
                     {
-                        throw new NotImplementedException(identifier);
+                        case GamePadBrowser.Chrome:
+                            if (identifier.Contains("Vendor: 045e")) // Microsoft
+                            {
+                                if (identifier.Contains("Product: 028e") || identifier.Contains("Product: 02a1")) // XBox 360 controller
+                                {
+                                    gamepadState.ReadAsMacintoshFirefoxXBox360(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else if (identifier.Contains("Vendor: 054c")) // Sony
+                            {
+                                if (identifier.Contains("Product: 0268")) // Playstation 3 controller
+                                {
+                                    gamepadState.ReadAsMacintoshChromePS3(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else if (identifier.Contains("Vendor: 046d")) // Logitech
+                            {
+                                if (identifier.Contains("Product: c216")) // F310 controller
+                                {
+                                    gamepadState.ReadAsMacintoshChromeF310(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else
+                            {
+                                throw new NotImplementedException(identifier);
+                            }
+                            break;
+
+                        case GamePadBrowser.Firefox:
+                            if (identifier.Contains("45e-")) // Microsoft
+                            {
+                                if (identifier.Contains("28e-") || identifier.Contains("2a1-")) // XBox 360 controller
+                                {
+                                    gamepadState.ReadAsMacintoshChromeXBox360(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else if (identifier.Contains("54c-")) // Sony
+                            {
+                                if (identifier.Contains("268-")) // Playstation 3 controller
+                                {
+                                    gamepadState.ReadAsMacintoshFirefoxPS3(gamepad);
+                                }
+                                else
+                                {
+                                    throw new NotImplementedException(identifier);
+                                }
+                            }
+                            else
+                            {
+                                throw new NotImplementedException(identifier);
+                            }
+                            break;
+
+                        case GamePadBrowser.Unknown:
+                            throw new NotSupportedException("Unsupported browser.");
                     }
-                }
-                else if (IsFirefox)
-                {
-                    if (identifier.Contains("45e-")) // Microsoft
-                    {
-                        if (identifier.Contains("28e-") || identifier.Contains("2a1-")) // XBox 360 controller
-                        {
-                            gamepadState.ReadAsWindowsFirefoxXBox360(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else if (identifier.Contains("46d-")) // Logitech
-                    {
-                        if (identifier.Contains("c21d-")) // F310 controller
-                        {
-                            gamepadState.ReadAsWindowsFirefoxF310(gamepad);
-                        }
-                        else if (identifier.Contains("c21e-")) // F510 controller
-                        {
-                            gamepadState.ReadAsWindowsFirefoxF510(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else
-                    {
-                        throw new NotImplementedException(identifier);
-                    }
-                }
-                else
-                {
-                    throw new NotSupportedException("Unsupported browser.");
-                }
-            }
-            else if (IsMacintosh)
-            {
-                if (IsChrome)
-                {
-                    if (identifier.Contains("Vendor: 045e")) // Microsoft
-                    {
-                        if (identifier.Contains("Product: 028e") || identifier.Contains("Product: 02a1")) // XBox 360 controller
-                        {
-                            gamepadState.ReadAsMacintoshFirefoxXBox360(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else if (identifier.Contains("Vendor: 054c")) // Sony
-                    {
-                        if (identifier.Contains("Product: 0268")) // Playstation 3 controller
-                        {
-                            gamepadState.ReadAsMacintoshChromePS3(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else if (identifier.Contains("Vendor: 046d")) // Logitech
-                    {
-                        if (identifier.Contains("Product: c216")) // F310 controller
-                        {
-                            gamepadState.ReadAsMacintoshChromeF310(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else
-                    {
-                        throw new NotImplementedException(identifier);
-                    }
-                }
-                else if (IsFirefox)
-                {
-                    if (identifier.Contains("45e-")) // Microsoft
-                    {
-                        if (identifier.Contains("28e-") || identifier.Contains("2a1-")) // XBox 360 controller
-                        {
-                            gamepadState.ReadAsMacintoshChromeXBox360(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else if (identifier.Contains("54c-")) // Sony
-                    {
-                        if (identifier.Contains("268-")) // Playstation 3 controller
-                        {
-                            gamepadState.ReadAsMacintoshFirefoxPS3(gamepad);
-                        }
-                        else
-                        {
-                            throw new NotImplementedException(identifier);
-                        }
-                    }
-                    else
-                    {
-                        throw new NotImplementedException(identifier);
-                    }
-                }
-                else
-                {
-                    throw new NotSupportedException("Unsupported browser.");
-                }
-            }
-            else
-            {
-                throw new NotSupportedException("Unsupported operating system.");
+                    break;
+
+                case GamePadOperatingSystem.Unknown:
+                    throw new NotSupportedException("Unsupported operating system.");
             }
 
             return gamepadState;
         }
 
-        public static GamePadState GetState(Int32 index)
+        private enum GamePadOperatingSystem
         {
-            return GamePad.GetState(index, HtmlPage.Window);
+            Unknown,
+            Windows,
+            Macintosh
+        }
+
+        private enum GamePadBrowser
+        {
+            Unknown,
+            Chrome,
+            Firefox
         }
     }
 }
--- TinkerWorX.Silverlight.InputSample/MainPage.xaml.cs
+++ TinkerWorX.Silverlight.InputSample/MainPage.xaml.cs
@@ -3,20 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Windows;
+using System.Windows.Browser;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using TinkerWorX.Silverlight.Input;
 
 namespace TinkerWorX.Silverlight.InputSample
 {
     public partial class MainPage : UserControl
     {
+        private Int32 index = 0;
+
         public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            CompositionTarget.Rendering += CompositionTarget_Rendering;
+        }
+
+        void CompositionTarget_Rendering(Object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(delegate()
+            {
+                var gamepad = GamePad.GetState(index);
+                if (gamepad.IsActive)
+                {
+                    Dispatcher.BeginInvoke(delegate()
+                    {
+                        this.LeftStickPointer.SetValue(Canvas.LeftProperty, 50.00 + gamepad.LeftStickX * 50 - this.LeftStickPointer.Width / 2.00);
+                        this.LeftStickPointer.SetValue(Canvas.TopProperty, 50.00 + gamepad.LeftStickY * 50 - this.LeftStickPointer.Height / 2.00);
+                        this.LeftStickValue.Text = "{" + gamepad.LeftStickX.ToString("0.00") + "; " + gamepad.LeftStickY.ToString("0.00") + "}";
+
+                        this.RightStickPointer.SetValue(Canvas.LeftProperty, 50.00 + gamepad.RightStickX * 50 - this.RightStickPointer.Width / 2.00);
+                        this.RightStickPointer.SetValue(Canvas.TopProperty, 50.00 + gamepad.RightStickY * 50 - this.RightStickPointer.Height / 2.00);
+                        this.RightStickValue.Text = "{" + gamepad.RightStickX.ToString("0.00") + "; " + gamepad.RightStickY.ToString("0.00") + "}";
+                    });
+                }
+            });
         }
     }
 }
